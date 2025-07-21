921aff0bcb25cb2a135f127dc8c1342026da5fc2
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-07-21 22:28:25 +0900

    Add authentication logic and loading state to auth component

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
@@ -7,7 +8,14 @@ export const meta = () => [
 ];
 
 const auth = () => {
-  const { isLoading } = usePuterStore();
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next]);
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
@@ -16,6 +24,26 @@ const auth = () => {
           <div className="flex flex-col gap-2 text-center items-center justify-center">
             <h1>Welcome</h1>
             <h2>Log in to continue your job journey</h2>
+          </div>
+
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in ...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Log out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Log in</p>
+                  </button>
+                )}
+              </>
+            )}
           </div>
         </section>
       </div>
